0b206fc95217fe9b2b48742f6db76b73e3508456
zVoid24 <rabbi1272@gmail.com>
2025-10-07 18:15:15 +0600

    ..

--- rest/middlewares/logger.go
+++ rest/middlewares/logger.go
@@ -9,8 +9,17 @@ import (
 func (h *Handler) Logger(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		start := time.Now()
-		log.Printf("%s %s %s", r.Method, r.RequestURI, r.RemoteAddr)
+
+		// Get the User-Agent from the request headers
+		userAgent := r.UserAgent()
+
+		// Log the method, URI, remote address, and User-Agent
+		log.Printf("%s %s %s %s", r.Method, r.RequestURI, r.RemoteAddr, userAgent)
+
+		// Call the next handler in the chain
 		next.ServeHTTP(w, r)
+
+		// Log the time taken to process the request
 		log.Printf("Completed in %v", time.Since(start))
 	})
 }
